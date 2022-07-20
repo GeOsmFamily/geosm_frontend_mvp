18f0df7349d2860ddc58dd0d3a57ea0e7ffb4f0d
BorisGautier <tchoukouahaboris@outlook.fr>
2022-07-20 20:40:35 +0100

    update test routing service

--- src/app/layouts/sidebar-layout/sidebar-right/routing/service/routing.service.spec.ts
+++ src/app/layouts/sidebar-layout/sidebar-right/routing/service/routing.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { RoutingService } from './routing.service';
@@ -6,7 +7,10 @@ describe('RoutingService', () => {
   let service: RoutingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [RoutingService]
+    });
     service = TestBed.inject(RoutingService);
   });
 
