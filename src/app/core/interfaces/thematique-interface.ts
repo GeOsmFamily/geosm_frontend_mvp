7d7c75cd83444224aef16fe223f2c828a8653a8e
BorisGautier <tchoukouahaboris@outlook.fr>
2022-07-21 12:41:29 +0100

    add legend

--- src/app/core/interfaces/thematique-interface.ts
+++ src/app/core/interfaces/thematique-interface.ts
@@ -1,82 +1,68 @@
 
+  export interface Couche {
+    id: number;
+    sous_thematique_id: number;
+    nom: string;
+    nom_en: string;
+    geometry: string;
+    schema_table_name: string;
+    remplir_color: string;
+    contour_color: string;
+    service_carto: string;
+    identifiant: string;
+    wms_type: string;
+    logo: string;
+    sql: string;
+    condition: string;
+    mode_sql: boolean;
+    sql_complete?: any;
+    opacite: string;
+    qgis_url: string;
+    bbox: string;
+    projection: string;
+    number_features: number;
+    vues: number;
+    surface: string;
+    distance: string;
+    telechargement: number;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at?: any;
+  }
 
+  export interface SousThematique {
+    id: number;
+    thematique_id: number;
+    nom: string;
+    nom_en: string;
+    image_src: string;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at?: any;
+    couches: Couche[];
+  }
 
-    export interface Proprietes {
-        id: number;
-        couche_id: number;
-        instance_id: number;
-        opacite: string;
-        qgis_url: string;
-        bbox: string;
-        projection: string;
-        number_features: number;
-        vues: number;
-        surface: string;
-        distance: string;
-        telechargement: number;
-        created_at?: any;
-        updated_at?: any;
-        deleted_at?: any;
-    }
+  export interface Thematique {
+    id: number;
+    nom: string;
+    nom_en: string;
+    image_src: string;
+    schema: string;
+    color: string;
+    ordre: number;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at?: any;
+    sous_thematiques: SousThematique[];
+  }
 
-    export interface Couche {
-        id: number;
-        sous_thematique_id: number;
-        nom: string;
-        nom_en: string;
-        geometry: string;
-        schema_table_name: string;
-        remplir_color: string;
-        contour_color: string;
-        service_carto: string;
-        identifiant: string;
-        wms_type: string;
-        logo: string;
-        sql: string;
-        condition: string;
-        mode_sql: boolean;
-        sql_complete: string;
-        created_at?: any;
-        updated_at?: any;
-        deleted_at?: any;
-        proprietes: Proprietes;
-    }
+  export interface Data {
+    thematiques: Thematique[];
+  }
 
-    export interface SousThematique {
-        id: number;
-        thematique_id: number;
-        nom: string;
-        nom_en: string;
-        image_src?: any;
-        created_at: Date;
-        updated_at: Date;
-        deleted_at?: any;
-        couches: Couche[];
-    }
-
-    export interface Thematique {
-        id: number;
-        nom: string;
-        nom_en: string;
-        image_src: string;
-        schema: string;
-        color: string;
-        ordre: number;
-        created_at: Date;
-        updated_at: Date;
-        deleted_at?: any;
-        sous_thematiques: SousThematique[];
-    }
-
-    export interface Data {
-        thematiques: Thematique[];
-    }
-
-    export interface ThematiqueInterface {
-      success: boolean;
-      data: Data;
-      message: string;
-    }
-
-
+  export interface ThematiqueInterface {
+    success: boolean;
+    data: Data;
+    message: string;
+  }
 
--- src/app/layouts/sidebar-layout/sidebar-right/legend/legend.component.ts
+++ src/app/layouts/sidebar-layout/sidebar-right/legend/legend.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { Map } from 'src/app/core/modules/openlayers';
+import { Component, Input, OnInit } from '@angular/core';
+import { LayersInMap } from '../../map/interfaces/layerinmap';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectAllLayersInToc } from '../../map/states/map.selector';
+import { ThematiqueService } from 'src/app/core/services/geosm/thematique/thematique.service';
+import { CarteService } from 'src/app/core/services/geosm/carte/carte.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-legend',
   templateUrl: './legend.component.html',
   styleUrls: ['./legend.component.scss']
 })
-export class LegendComponent  {
+export class LegendComponent implements OnInit {
+  @Input() map: Map | undefined;
 
-  constructor() { }
+  layersInToc$: Observable<LayersInMap[]>;
 
+  layersInTocWithLegend: Array<LayersInMap> = [];
+
+  constructor(
+    private store: Store,
+    public domSanitizer: DomSanitizer,
+    private thematiqueService: ThematiqueService,
+    private carteService: CarteService
+  ) {
+    this.layersInToc$ = this.store.select(selectAllLayersInToc);
+  }
+  ngOnInit(): void {
+    this.map?.getLayers().on('propertychange', _ObjectEvent => {
+      this.getAllLayersForToc();
+    });
+  }
+
+  getAllLayersForToc() {
+    this.layersInToc$.subscribe(layersInToc => {
+      let reponseLayers: Array<LayersInMap> = [];
+      for (let index = 0; index < layersInToc.length; index++) {
+        const layerProp = layersInToc[index];
+        if (layerProp.legendCapabilities && layerProp['type_layer'] == 'geosmCatalogue') {
+          if (layerProp.legendCapabilities.useCartoServer) {
+            let url;
+            let identifiant;
+            if (layerProp['properties']!['type'] == 'couche') {
+              let couche = this.thematiqueService.getCoucheFromId(layerProp['properties']!['couche_id']);
+              url = couche ? couche.qgis_url : undefined;
+              identifiant = couche ? couche.identifiant : undefined;
+            } else if (layerProp['properties']!['type'] == 'carte') {
+              let carte = this.carteService.getCarteById(layerProp['properties']!['couche_id']);
+              url = carte ? carte.url : undefined;
+              identifiant = carte! ? carte.identifiant : undefined;
+            }
+            if (url && identifiant) {
+              let legendUrl =
+                url +
+                '&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=' +
+                identifiant +
+                '&STYLE=default&SLD_VERSION=1.1.0&LAYERTITLE=false&RULELABEL=true';
+              layerProp.legendCapabilities.urlImg = legendUrl.trim();
+            }
+          }
+
+          if (layerProp.legendCapabilities.urlImg) {
+            reponseLayers.push(layerProp);
+          }
+        }
+      }
+
+      function compare(a: { zIndex: number }, b: { zIndex: number }) {
+        if (a.zIndex < b.zIndex) {
+          return 1;
+        }
+        if (a.zIndex > b.zIndex) {
+          return -1;
+        }
+        return 0;
+      }
+      this.layersInTocWithLegend = reponseLayers;
+
+      this.layersInTocWithLegend.sort(compare);
+    });
+  }
 }
