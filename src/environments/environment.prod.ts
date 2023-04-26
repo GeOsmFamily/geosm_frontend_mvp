3e31ac43f60e585068fa4446e14b6c9ef0aba1f1
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2023-04-26 14:14:30 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  url_services: 'https://eneoservices.position.cm',
-  apiKey: 'GKbSQ0BBm1PHiZO0M4nITAl3Wb1KH6Hp8bR7Lwb8haDedZQzKWKcD64lC2TtstKk',
+  url_services: 'https://bamakoservices.position.cm',
+  apiKey: 'RGpKEijHA8iGA0HP252kk9JxKVeq0Yt5zjakBCWheOoWbvKjrulsAG9now0kWh46',
   instance_id: 1,
   primarycolor: '#023f5f',
-  nom: 'eneo',
+  nom: 'bamako',
   url_frontend: 'http://localhost:4200',
-  country_code: 'CM',
+  country_code: 'ML',
   url_carto: 'https://carto.position.cm',
   url_qgis: 'https://qgis.position.cm'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   url_services: 'https://bamakoservices.position.cm',
-  apiKey: 'vGhKBs2fR7GbdBZTjZUQsTGQPAj8RUzPdLXGnNnlUZpFsyEE8xAgFhBxnFqYpUH9',
+  apiKey: 'RGpKEijHA8iGA0HP252kk9JxKVeq0Yt5zjakBCWheOoWbvKjrulsAG9now0kWh46',
   instance_id: 1,
   primarycolor: '#023f5f',
-  nom: 'eneo',
+  nom: 'bamako',
   url_frontend: 'http://localhost:4200',
-  country_code: 'CM',
+  country_code: 'ML',
   url_carto: 'https://carto.position.cm',
   url_qgis: 'https://qgis.position.cm'
 };
