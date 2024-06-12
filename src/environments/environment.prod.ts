3dbec8433a9025450aaacb72bb81f6e04f5d112e
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2024-06-12 15:48:22 +0100

    Update API endpoint URLs to use the new domain position.cm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  url_services: 'https://camerounservices.position.cm',
-  apiKey: 'd3G6KNuMSo77Nn5BKUVOPErcckLcWN1TF9tmqZtT9LLKnZUHTOT9TSFtRfDUzB5m',
+  url_services: 'https://soaservices.position.cm',
+  apiKey: 'ujwQtI4KcBUjj1QKHykIxD1HK8gbjlQfrJuTMfGg2geqpVOdAf7z7jpWmlR7C8yW',
   instance_id: 1,
   primarycolor: '#023f5f',
-  nom: 'cameroun',
-  url_frontend: 'https://cameroun.position.cm',
+  nom: 'soa',
+  url_frontend: 'https://soa.position.cm',
   country_code: 'CM',
   url_carto: 'https://carto.position.cm',
   url_qgis: 'https://qgis.position.cm'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  url_services: 'https://camerounservices.position.cm',
-  apiKey: 'd3G6KNuMSo77Nn5BKUVOPErcckLcWN1TF9tmqZtT9LLKnZUHTOT9TSFtRfDUzB5m',
+  url_services: 'https://soaservices.position.cm',
+  apiKey: 'ujwQtI4KcBUjj1QKHykIxD1HK8gbjlQfrJuTMfGg2geqpVOdAf7z7jpWmlR7C8yW',
   instance_id: 1,
   primarycolor: '#023f5f',
-  nom: 'cameroun',
+  nom: 'soa',
   url_frontend: 'http://localhost:4200',
   country_code: 'CM',
   url_carto: 'https://carto.position.cm',
