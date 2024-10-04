0341bbdd7552dd09f0beb3f081e093e7704771b4
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2024-10-04 11:43:06 +0100

    Update API endpoint URLs to use the new domain position.cm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  url_services: 'https://soaservices.position.cm',
+  url_services: 'https://pucservices.position.cm',
   apiKey: 'Yo3CzYS6xRG1RUkTnhtcAGp1lEw4W03cIf4cgPGOl6EvN8pQ2kfiQ9ENA9J6aDnp',
   instance_id: 1,
   primarycolor: '#023f5f',
-  nom: 'soa',
-  url_frontend: 'https://soa.position.cm',
+  nom: 'puc',
+  url_frontend: 'https://puc.position.cm',
   country_code: 'CM',
   url_carto: 'https://carto.position.cm',
   url_qgis: 'https://qgis.position.cm'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  url_services: 'https://soaservices.position.cm',
+  url_services: 'https://pucservices.position.cm',
   apiKey: 'Yo3CzYS6xRG1RUkTnhtcAGp1lEw4W03cIf4cgPGOl6EvN8pQ2kfiQ9ENA9J6aDnp',
   instance_id: 1,
   primarycolor: '#023f5f',
-  nom: 'soa',
+  nom: 'puc',
   url_frontend: 'http://localhost:4200',
   country_code: 'CM',
   url_carto: 'https://carto.position.cm',
