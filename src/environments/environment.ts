10634b8f6c74433bf87c5b731ec1fe6711a5bca5
dzoibo <ivanztitan@gmail.com>
2024-11-25 14:50:24 +0100

    fix: add title

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   url_services: 'https://pucservices.position.cm',
-  apiKey: 'E9DG0m6Q8vQSuGv6PPlW5dwKTGnXAaMoeZa8LMGN9oYRwRNqMh6ZYkj5432Qdbhw',
+  apiKey: 'VQE6hycrFBnWcBQ5gqBvyhUzYqLf45Ap5vetcZnPe0Ft06EnmMWW47eSfNnixdhw',
   instance_id: 1,
   primarycolor: '#023f5f',
   nom: 'puc',
